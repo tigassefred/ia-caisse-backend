7028760912741fc33729bcbec6a4dc1031cfc9c5
tigassefred <tigassefred@gmail.com>
2024-11-30 03:04:11 +0000

    wp

--- routes/console.php
+++ routes/console.php
@@ -8,13 +8,13 @@ use Illuminate\Support\Facades\Artisan;
 //})->purpose('Display an inspiring quote')->hourly();
 
 
-\Illuminate\Support\Facades\Schedule::command('app:update-defer-pay')->dailyAt('02:28');
-\Illuminate\Support\Facades\Schedule::command('app:close-casher')->dailyAt('02:34');
+//\Illuminate\Support\Facades\Schedule::command('app:update-defer-pay')->dailyAt('02:28');
+//\Illuminate\Support\Facades\Schedule::command('app:close-casher')->dailyAt('02:34');
 
-\Illuminate\Support\Facades\Schedule::command('app:test')->dailyAt('01:23');
-\Illuminate\Support\Facades\Schedule::command('app:test')->dailyAt('01:25');
-\Illuminate\Support\Facades\Schedule::command('app:test')->dailyAt('01:30');
-\Illuminate\Support\Facades\Schedule::command('app:test')->dailyAt('01:36');
-\Illuminate\Support\Facades\Schedule::command('app:test')->dailyAt('01:37');
-\Illuminate\Support\Facades\Schedule::command('app:test')->dailyAt('01:43');
+\Illuminate\Support\Facades\Schedule::command('app:test')->dailyAt('03:08');
+\Illuminate\Support\Facades\Schedule::command('app:test')->dailyAt('03:09');
+\Illuminate\Support\Facades\Schedule::command('app:test')->dailyAt('01:11');
+\Illuminate\Support\Facades\Schedule::command('app:test')->dailyAt('01:13');
+\Illuminate\Support\Facades\Schedule::command('app:test')->dailyAt('01:15');
+\Illuminate\Support\Facades\Schedule::command('app:test')->dailyAt('01:17');
 
